47815f42520c2b79180ceb066af53b340221c551
Alexander Wachenfeld <wachenfeld.alexander@gmail.com>
2023-11-10 16:31:37 +0100

    Pushed cuboid instantiation for the particles

--- src/ParticleGenerator.cpp
+++ src/ParticleGenerator.cpp
@@ -1,1 +1,30 @@
-//TODO: Implement
+/**
+ * @brief: generates a cuboid of particles specified by the user input
+ */
+#include "ParticleGenerator.h"
+
+void ParticleGenerator::instantiateCuboid(ParticleContainer &container, std::array<double, 3> llfc,
+                                          std::array<unsigned int, 3> particlePerDimension, double h, double mass,
+                                          std::array<double, 3> particleVelocity) {
+    double meanValueVelocity{0.0}; // TODO: Is it the Erwartungswert of the normal distribution
+    std::array<double, 3> mbVelocity = maxwellBoltzmannDistributedVelocity(meanValueVelocity, 3);
+    std::vector<Particle> particles_temp; //TODO: reserve space for the vector for Performance
+    for (int i = 0; i < particlePerDimension[0]; ++i) {
+        for (int j = 0; j < particlePerDimension[1]; ++j) {
+            for (int k = 0; k < particlePerDimension[2]; ++k) {
+                std::array<double, 3>  x_arg{i*h, j*h, k*h};
+                auto v_arg = particleVelocity + mbVelocity;
+                //std::array<double, 3> v_arg{particleVelocity + mbVelocity}; // Calculate via the Brownian motion
+                particles_temp.emplace_back(Particle{x_arg, v_arg, mass, 0});
+            }
+        }
+    }
+    container.setParticles(particles_temp);
+}
+
+/* Use this constructor for the initialization
+Particle::Particle(std::array<double, 3> x_arg, std::array<double, 3> v_arg, double m_arg, int type_arg):x(x_arg), v(v_arg), m(m_arg), type(type_arg) {
+    f = {0., 0., 0.};
+    old_f = {0., 0., 0.};
+    std::cout << "Particle generated!" << std::endl;
+}*/
--- src/ParticleGenerator.h
+++ src/ParticleGenerator.h
@@ -1,2 +1,23 @@
 #pragma once
+#include <array>
+#include <vector>
+#include "ParticleContainer.h"
+#include "Particle.h"
+#include "utils/MaxwellBoltzmannDistribution.h"
+#include "utils/ArrayUtils.h"
 
+class ParticleGenerator {
+private:
+    /*std::array<double, 3> llfc; // lower left frontside corner
+    std::array<double, 3> particlePerDimension;
+    double h;
+    double mass;
+    std::array<double, 3> particleVelocity;*/
+    //TODO: last parameter can be hardcoded but maybe add later
+
+    //TODO: Hier werden wir einen Cuboid initialisieren
+public:
+    void instantiateCuboid(ParticleContainer& container, std::array<double, 3> llfc, std::array<unsigned int, 3> particlePerDimension, double h, double mass,
+                           std::array<double, 3> particleVelocity);
+
+};
