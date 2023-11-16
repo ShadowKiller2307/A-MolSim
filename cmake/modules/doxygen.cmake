8727b2faf225a70d37cec132dfd730ffd2323dfb
Felix Huber <ge24cob@mytum.de>
2023-11-16 16:32:43 +0100

    some improvements

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -8,9 +8,7 @@ if (BUILD_DOXY)
                 doc_doxygen
                 COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-				
         )
-
         set_target_properties(doc_doxygen PROPERTIES EXCLUDE_FROM_ALL TRUE)
     endif (DOXYGEN_FOUND)
 endif (BUILD_DOXY)
