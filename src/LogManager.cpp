a2b72579a2b47803d4d18ce1ad0b1ccede0ead2f
Felix Huber <ge24cob@mytum.de>
2023-11-20 18:31:14 +0100

    fixed path

--- src/LogManager.cpp
+++ src/LogManager.cpp
@@ -1,29 +1,33 @@
 
 #include "LogManager.h"
 
-LogManager &LogManager::getInstance() {
+LogManager &LogManager::getInstance()
+{
     static LogManager instance;
     return instance;
 }
 
-LogManager::LogManager() {
+LogManager::LogManager()
+{
 
     logLevel = spdlog::level::info;
     spdlog::set_pattern("[%Y-%m-%d %H:%M] [%l] %v");
-    logger = spdlog::rotating_logger_mt("fileLogger", "/logs/log.txt",
+    logger = spdlog::rotating_logger_mt("fileLogger", "../logs/log.txt",
                                         maxSize, maxFiles, true);
     logger->set_level(this->logLevel);
 }
 
-std::shared_ptr<spdlog::logger> LogManager::getLogger() {
+std::shared_ptr<spdlog::logger> LogManager::getLogger()
+{
     return logger;
 }
 
-spdlog::level::level_enum LogManager::getLevel() {
+spdlog::level::level_enum LogManager::getLevel()
+{
     return this->logLevel;
 }
 
-void LogManager::setLogLevel(spdlog::level::level_enum level) {
+void LogManager::setLogLevel(spdlog::level::level_enum level)
+{
     this->logLevel = level;
 }
-
