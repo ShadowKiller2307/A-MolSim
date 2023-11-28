41a88cc35afaa4b57b1c8cd749428c36c4078e9e
Felix Huber <ge24cob@mytum.de>
2023-11-28 16:14:51 +0100

    small typo

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -85,7 +85,7 @@ int main(int argc, char *const argv[])
 	// c++ can't do switch statements on strings😔, this does the job, let's not overcomplicate things
 	if (ending == "xml")
 	{
-		path = std::string("_.json").compare(argv[optind]) == 0 ? "../input/default.xml" : argv[optind];
+		path = std::string("_.xml").compare(argv[optind]) == 0 ? "../input/default.xml" : argv[optind];
 		particleGenerator::instantiateXML(&container, path, opts);
 	}
 	else if (ending == "json")
