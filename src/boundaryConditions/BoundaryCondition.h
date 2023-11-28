c52020954e7fe3558d410161a1d6d7181441f640
Alexander Wachenfeld <wachenfeld.alexander@gmail.com>
2023-11-28 17:35:54 +0100

    Added LinCel container and its method, added the boundaryCondition classes but not implemented yet

---
+++ src/boundaryConditions/BoundaryCondition.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class BoundaryCondition {
+
+};
+
+
+#endif //PSEMOLDYN_GROUPA_BOUNDARYCONDITION_H
--- src/particleContainers/ParticleContainerLinCel.h
+++ src/particleContainers/ParticleContainerLinCel.h
@@ -1,13 +1,50 @@
 #pragma once
 #include "particleContainers/ParticleContainer.h"
+#include "boundaryConditions/BoundaryCondition.h"
 using cell = std::vector<Particle>;
 
 class ParticleContainerLinCel : ParticleContainer
 {
 private:
 	std::vector<cell> cells;
+    std::vector<BoundaryCondition> conditions_;
+    using cell = std::vector<Particle>;
+    double amountOfCells = (domainSize_[0]/cutoffRadius_) * (domainSize_[1]/cutoffRadius_) * (domainSize_[2]/cutoffRadius_) //inner+boundary cells
+                           + 2 * cellsX + 2 * (cellsY-2); // adding the halo cells
+    /**
+    * @brief the cells can be divided into inner, boundary and halo cells
+    *
+    * the first element of the cells is the cell at the front lower left corner of the domain
+    * cells go from:
+    * ->left to right
+    * ->front to back
+    * ->from down to up
+    */
+    std::array<double, 3> domainSize_;
+    /**
+     * 2D cell: cutoffRadius * cutoffRadius * 1
+     * 3D cell: cutoffRadius * cutoffRadius * cutoffRadius
+     */
+    double cutoffRadius_;
+    /*
+     * amount of Cells in each dimension can only be integer
+     */
+    int cellsX = static_cast<int> (domainSize_[0]/cutoffRadius_);
+    int cellsY = static_cast<int> (domainSize_[1]/cutoffRadius_);
+    int cellsZ = static_cast<int> (domainSize_[2]/cutoffRadius_);
 
 public:
-	ParticleContainerLinCel(/* args */);
+	ParticleContainerLinCel(double deltaT, double endTime, std::array<double, 3> domainSize, double cutoffRadius, std::vector<BoundaryCondition> &conditions);
 	~ParticleContainerLinCel();
+    void iterOverInnerPairs(const std::function<void(Particle &a, Particle &b)> &f) override;
+    /**
+     * @brief iterate over the particles which are currectly located in the boundary zone
+     *  @param boundaryLambda an array of 4 BoundaryConditions so that a different boundary condition can be applied to each side
+     */
+    void add(const std::array<double, 3> &x_arg, const std::array<double, 3> &v_arg, double mass, int type) override;
+    void iterBoundary(std::array <const std::function<void(Particle &, Particle &)>, 4> &boundaryLambda);
+    /**
+     * @brief iterate over the particles which are currectly located in the halo zone
+     */
+    void iterHalo();
 };
